21bd283285e40b9cc8684a5e812b4f458c689c1c
Sirojiddin Aripov <aripov86cm@gmail.com>
2024-02-26 20:39:33 -0500

    Added user class with creation test cases

---
+++ app/src/test/java/com/aripov/unittesting_ui_elements/UserTest.kt
@@ -0,0 +1,34 @@
+package com.aripov.unittesting_ui_elements
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Assert.*
+
+import org.junit.Test
+import org.junit.jupiter.api.assertThrows
+
+class UserTest {
+
+    @Test
+    fun `normal creation of user returns true`() {
+        val email = "example@example.com"
+        val password = "Example11"
+        val result = User(email, password).showUserInfo()
+        assertThat(result).isEqualTo("$email\n${password}")
+    }
+    @Test
+    fun `invalid email creation of user throws exception`() {
+        assertThrows<IllegalArgumentException> {
+            val email = "example"
+            val password = "Example11"
+            User(email, password).showUserInfo()
+        }
+    }
+    @Test
+    fun `invalid password creation of user throws exception`() {
+        assertThrows<IllegalArgumentException> {
+            val email = "example"
+            val password = ""
+            User(email, password).showUserInfo()
+        }
+    }
+}
